207c57f22ef031b1c64b0958f8003716ec815309
Joe McCorison <joemccorison@gmail.com>
2019-09-28 21:20:55 +0100

    additonal comments

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,7 +1,13 @@
+/**
+ * Finds absolute middle of an array and returns the middle -1
+ * @todo add test to support results
+ * @param a Array of numbers
+ */
 function absoluteValuesSumMinimization(a: number[]): number {
-  const isEven = a.length % 2 === 0; // determine if even or odd
+  const isEven = a.length % 2 === 0; // even / odd
 
-  // get the middle and go -1 of middle
+  // isEven then find the index that is half -1 of middle
+  // isOdd find the absolute middle index
   return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
 }
 
